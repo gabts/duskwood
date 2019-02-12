892eeb9cc961d2f9d2ac7c48bc0af37f6915928f
gabbes <gabtost@gmail.com>
2019-02-12 18:13:50 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Dusky
+
+> A dusky theme for [Visual Studio Code](http://code.visualstudio.com).
+
+## Development
+
+Make desired change to theme by editing values in files under `src` directory.
+
+Generate finished theme file by running script `yarn build`.
+
+## License
+
+[MIT](./LICENSE)
---
+++ index.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const yaml = require('js-yaml');
+
+// Source files that should be combined to create theme. Note that load order
+// matters here because variables must be declared on above any references.
+const src = [
+  { key: 'variables', path: './src/theme.yaml' },
+  { key: 'colors', path: './src/colors.yaml' },
+  { key: 'tokenColors', path: './src/tokens.yaml' }
+];
+
+// Combine source contents into a single Yaml string. Because values
+// are wrapped by an object key all new lines are indented two spaces.
+const str = src.reduce((acc, { key, path }) => {
+  const values = fs.readFileSync(path, 'utf8');
+
+  acc += `${key}:\n  ${values.replace(/\n/g, '\n  ')}\n`;
+
+  return acc;
+}, '');
+
+// Parse Yaml string into a JavaScript object.
+const obj = yaml.safeLoad(str);
+
+// Filter null key values from colors object.
+const colors = Object.keys(obj.colors).reduce((acc, key) => {
+  if (obj.colors[key] !== null) {
+    acc[key] = obj.colors[key];
+  }
+
+  return acc;
+}, {});
+
+// Construct final theme object.
+const theme = {
+  name: 'Dusky',
+  author: 'Gabriel Toll Stålbom',
+  type: 'dark',
+  colors,
+  tokenColors: obj.tokenColors
+};
+
+// Write theme Json distribution file.
+fs.writeFileSync('./theme/dusky.json', `${JSON.stringify(theme, null, 2)}\n`);
